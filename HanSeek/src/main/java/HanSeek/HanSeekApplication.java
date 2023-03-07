ef0d77e8aee11afbd636407f61826f2f512039cb
Seungwoo Ko <78254737+jollidah@users.noreply.github.com>
2023-03-08 00:42:48 +0900

    Add files via upload

---
+++ HanSeek/src/main/java/HanSeek/HanSeekApplication.java
@@ -0,0 +1,13 @@
+package HanSeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanSeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanSeekApplication.class, args);
+	}
+
+}
---
+++ HanSeek/src/test/java/HanSeek/HanSeekApplicationTests.java
@@ -0,0 +1,13 @@
+package HanSeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanSeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
