0a61c5f084a21ec0429af2c2f1e54bda0af945f1
jollidah <help0304@naver.com>
2023-03-09 03:55:45 +0900

    Implement base function of survey

--- HanSeek/src/main/java/HanSeek/Database/answerMap.java
+++ HanSeek/src/main/java/HanSeek/Database/answerMap.java
@@ -1,17 +1,59 @@
 package HanSeek.Database;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
-public class answerMap {
-    private static HashMap <String, JSONObject> answerMap = new HashMap<>();
-    private static long ID;
 
-    public static HashMap<String, JSONObject> getAnswerMap() {
-        return answerMap;
+@Component
+public class answerMap implements answer{
+
+    private static HashMap <Long, JSONObject> answerMap = new HashMap<>();
+    private static long id = 0L;
+
+    @Override
+    public JSONObject getById(long inputId) {
+        return answerMap.get(inputId);
     }
 
-//    public static void setAnswerMap(HashMap<String, JSONObject> answerMap) {
-//        answerMap.answerMap = answerMap;
-//    }
+    @Override
+    public long makeNewId(){
+        try {
+            JSONObject tmp = new JSONObject();
+            tmp.put("UserId", id);
+            answerMap.put(id, tmp);
+        } catch (Exception e){
+            System.out.println("ERROR while making new object");
+            e.printStackTrace();
+        }
+        return id++;
+    }
+
+    @Override
+    public void sendToModel(long inputId) {
+//        try{
+            System.out.println(getById(inputId).toString());
+//            String path = String.format("HanSeek/Database/DataToModel/%d.json", inputId);
+//            PrintWriter out = new PrintWriter(new FileWriter(path));
+//            out.write(getById(inputId).toString());
+//        }catch (IOException e){
+//            System.out.println("IOException while sending data to model");
+//            e.printStackTrace();
+//        }
+    }
+
+    @Override
+    public void addData(long inputId, String Question, int ans) {
+        try{
+            getById(inputId).put(Question, ans);
+        } catch (JSONException e) {
+            System.out.println("JSONJSONException - while adding data at JSONObject");
+            e.printStackTrace();
+        }
+    }
 }
